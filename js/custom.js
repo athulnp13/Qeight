c8f8340ec119fc7e3f6702e3b820ea600f6f26ef
athulnp13 <athuln10@gmail.com>
2024-09-29 22:55:30 +0530

    added images

--- js/custom.js
+++ js/custom.js
@@ -35,8 +35,57 @@ $(".owl-carousel").owlCarousel({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(24.3640348, 54.5013499),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+document.getElementById("success").style.visibility = "hidden";
+//Listen for submit
+document.querySelector('.contact-form').addEventListener("submit",submitform);
+function submitform(e){
+    e.preventDefault();
+
+    //Get inputs
+    let name = document.querySelector("#tbname").value;
+    let phone = document.querySelector("#tbphone").value;
+    let email = document.querySelector("#tbemail").value;
+    let message = document.querySelector("#tbmessage").value;
+    sendEmail(name,phone,email,message);
+    document.querySelector('.contact-form').reset();
+}
+
+function sendEmail(name,phone,email,message){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "equiryfromqmed@gmail.com",
+        Password : "qexvadaocwqcyfbc",
+        To : 'athuln10@gmail.com',
+        From : "equiryfromqmed@gmail.com",
+        Subject : `Enquiry from ${name}`,
+        Body : `Name: ${name}<br/> Phone: ${phone} <br/> Email: ${email}<br/> Message:${message}`
+        }).then((message) => showSuccessMessage());
+}
+var timeOutVar;
+
+function showSuccessMessage(){
+    document.getElementById("success").style.visibility = "visible";
+    document.getElementById("success").style.opacity = 1;
+    fadeOutEffect();
+}
+
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("success");
+     fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 1500);
+}
+  
